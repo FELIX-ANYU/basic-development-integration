0aa4e53d4317eb49f7127509d47b5381cea0b7db
fmw <1018856578@qq.com>
2020-08-29 16:12:02 +0800

    Add 新模块

---
+++ base-test/src/main/java/com/base/test/thread/Producer.java
@@ -0,0 +1,4 @@
+package com.base.test.thread;
+
+public class Producer {
+}
---
+++ base-test/src/main/java/com/base/test/thread/Thread.java
@@ -0,0 +1,8 @@
+package com.base.test.thread;
+
+public class Thread {
+
+    public static void main(String[] args) {
+
+    }
+}
